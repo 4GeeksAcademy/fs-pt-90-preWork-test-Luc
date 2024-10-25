82cfccc217b51f9606f5343fa33f872955e26b52
Luc <lucsanguineti@protonmail.com>
2024-10-25 17:58:26 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,38 +1,35 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
 /*
 
 <------------------------- INICIO SELECCION SIMPLE -------------------------->
 Pregunta 1: ✅
 ¿Cuál es la etiqueta correcta para insertar una imagen en HTML?
-a) <img src="ruta"> 
+a) <img src="ruta"> ✅
 b) <image src="ruta">
 
 Pregunta 2: ✅
 ¿Qué etiqueta HTML se utiliza para crear un enlace?
-a) <a href="url">Texto</a> 
+a) <a href="url">Texto</a> ✅
 b) <link href="url">Texto</link>
 
 Pregunta 3: ✅
 ¿Cuál de los siguientes selectores selecciona un elemento por su ID?
-a) #elemento 
+a) #elemento ✅
 b) .elemento
 
 Pregunta 4: ✅
 ¿Cuál es la propiedad CSS que se utiliza para cambiar el color del texto?
-a) color 
+a) color ✅
 b) text-color 
 
 Pregunta 5: ✅
 ¿Cuál propiedad se utiliza para convertir un contenedor en un contenedor flex?
-a) display: flex; 
+a) display: flex; ✅
 b) flex: container
 
 Pregunta 6: ✅
 ¿Qué valor de flex-direction coloca los elementos en una columna vertical?
-a) column 
+a) column ✅
 b) vertical
 
 Pregunta 7: ✅
@@ -42,7 +39,7 @@ a) space-between distribuye los elementos con espacios iguales entre ellos, deja
    mientras que space-around agrega espacio igual entre los elementos y en los bordes del contenedor.
 
 b) space-between coloca los elementos con espacio igual entre ellos, mientras que space-around distribuye el espacio uniformemente 
-   tanto entre los elementos como en los bordes
+   tanto entre los elementos como en los bordes ✅
 
 
 Pregunta 8: ✅
@@ -53,7 +50,7 @@ let numeroConvertido = parseInt(numero);
 Cual es el resultado de ejecutar un typeof numeroConvertido
 
 a) "string"
-b) "number"
+b) "number" ✅
 c) NaN
 d) "boolean"
 
@@ -65,7 +62,7 @@ Que sucede si ejecutamos frutas.push("pera");
 
 a) Se muestra un error porque el arreglo no puede modificarse.
 b) Se agrega "pera" al inicio del arreglo.
-c) Se agrega "pera" al final del arreglo.
+c) Se agrega "pera" al final del arreglo. ✅
 d) Se elimina "pera" del arreglo.
 
 Pregunta 10: ✅
@@ -73,7 +70,7 @@ si tenemos:
 const frutas = ["manzana", "banana", "naranja"];
 Cual seria el resultado de hacer frutas.pop();
 
-a) ["manzana", "banana"]
+a) ["manzana", "banana"] ✅
 b) ["manzana", "naranja"]
 c) ["banana", "naranja"]
 d) ["manzana", "banana", "naranja"]
@@ -87,8 +84,8 @@ Y ejecutamos saludo.toLowerCase()
 Cual sera el resultado?
 a) "HOLA mundo"
 b) "Hola mundo"
-c) "hola mundo"
-d) undefined
+c) "hola mundo" ✅
+d) undefined 
 
 Pregunta 12: ✅
 Que hara el siguiente bloque de codigo?
@@ -110,11 +107,9 @@ quePuedeHacer();
 
 opciones:
 a) Devolvera "No puedes viajar".
-b) Devolvera "Puedes viajar".
+b) Devolvera "Puedes viajar". ✅
 c) Devolvera un error de sintaxis.
 d) No Devolvera nada.
-
-
 
 
 <------------------------- FINAL SELECCION SIMPLE -------------------------->
@@ -122,9 +117,8 @@ d) No Devolvera nada.
 
 //----------------------- PARTE PRACTICA -----------------------------
 
-window.onload = function() {
-  // Pregunta 13:
-  /* Usando el siguiente arreglo:
+// Pregunta 13:
+/* Usando el siguiente arreglo:
   let frutas = ["manzana", "banana", "naranja", "piña"];
   Y con los conocimientos obtenidos en estas ultimas dos semanas.
   Imprime en consola el resultado de los siguientes pasos (debes por cada paso hacer un console.log para
@@ -133,17 +127,32 @@ window.onload = function() {
   b) Elimina el primer elemento.
   c) Reemplaza "naranja" con "kiwi". */
 
-  //write your code here
+//write your code here
 
-  // Pregunta 14:
-  /*Escribe una función en JavaScript que ordene un arreglo de números de menor a mayor sin utilizar métodos de arreglo como sort. 
+let frutas = ["manzana", "banana", "naranja", "piña"];
+
+// a)
+frutas.push("mango");
+console.log(frutas);
+
+// b)
+frutas.shift();
+console.log(frutas);
+
+// c) No recuerdo cómo hacer este ejercicio.
+frutas.splice(i, 0, "kiwi");
+console.log(frutas);
+
+// Pregunta 14:
+/*Escribe una función en JavaScript que ordene un arreglo de números de menor a mayor sin utilizar métodos de arreglo como sort. 
   Puedes usar bucles y condicionales básicos.*/
 
-  let numeros = [5, 3, 8, 4, 2];
-  function bubbleSort(arr) {
-    //write your code here
-    return;
-  }
+let numeros = [5, 3, 8, 4, 2];
+function bubbleSort(arr) {
+  //write your code here
+  return;
+}
 
-  console.log(bubbleSort(numeros)); // [2, 3, 4, 5, 8]
-};
+console.log(bubbleSort(numeros)); // [2, 3, 4, 5, 8]
+
+// No he llegado a estudiar este ejercicio.
